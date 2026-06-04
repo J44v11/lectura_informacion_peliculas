b6acfd7dd4a7b85460d91534a1a01ea1fec5e837
J44v11 <javiergarcialopez101@gmail.com>
2026-06-04 10:43:28 +0100

    feat: Añado clase Pelicula con atributos y constructor

---
+++ src/net/salesianos/pelicula/Pelicula.java
@@ -0,0 +1,15 @@
+package net.salesianos.pelicula;
+
+public class Pelicula {
+
+    private String titulo;
+    private String genero;
+    private int duracionMinutos;
+
+    public Pelicula(String titulo, String genero, int duracionMinutos) {
+        this.titulo = titulo;
+        this.genero = genero;
+        this.duracionMinutos = duracionMinutos;
+    }
+    
+}
