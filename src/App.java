bd82500a2f74eff451f1c6e866ba578dcb1a79bd
J44v11 <javiergarcialopez101@gmail.com>
2026-06-04 13:28:19 +0100

    feat: Agrego archivo CSV con información de películas y actualizo la clase App para leerlo

--- src/App.java
+++ src/App.java
@@ -1,5 +1,19 @@
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+
+        try {
+
+            String contenido = Files.readString(Path.of("peliculas.csv")); // Lectura del fichero completo
+
+            System.out.println(contenido); // Lo mostramos por consola para comprobar que funciona
+
+        } catch (IOException e) {
+            System.out.println("Error al leer el fichero: " + e.getMessage());
+        }
+
     }
 }
