d22ff3b1c6305b7d0b6dc3ad7812171e78f1f303
J44v11 <javiergarcialopez101@gmail.com>
2026-06-04 13:35:12 +0100

    feat: Actualizo la lectura del archivo CSV para procesar línea por línea y omito la cabecera

--- src/App.java
+++ src/App.java
@@ -1,16 +1,22 @@
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.List;
 
 public class App {
     public static void main(String[] args) throws Exception {
 
         try {
-
-            String contenido = Files.readString(Path.of("peliculas.csv")); // Lectura del fichero completo
-
-            System.out.println(contenido); // Lo mostramos por consola para comprobar que funciona
-
+            // Lee el fichero peliculas.csv línea a línea
+            List<String> lineas = Files.readAllLines(Path.of("peliculas.csv"));
+            
+            // Ignora la primera línea
+            // Verificamos que la lista no esté vacía por seguridad, y eliminamos la posición 0
+            if (!lineas.isEmpty()) {
+                String cabecera = lineas.remove(0);
+                System.out.println("Hemos ignorado esta cabecera: " + cabecera);
+            }
+            
         } catch (IOException e) {
             System.out.println("Error al leer el fichero: " + e.getMessage());
         }
