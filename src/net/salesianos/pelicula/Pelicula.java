63015aaca8b52091dc3dc9fb989f13bab0d51875
J44v11 <javiergarcialopez101@gmail.com>
2026-06-04 10:44:03 +0100

    feat: Añado métodos getter y setter a la clase Pelicula

--- src/net/salesianos/pelicula/Pelicula.java
+++ src/net/salesianos/pelicula/Pelicula.java
@@ -11,5 +11,28 @@ public class Pelicula {
         this.genero = genero;
         this.duracionMinutos = duracionMinutos;
     }
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getGenero() {
+        return genero;
+    }
     
+    public void setGenero(String genero) {
+        this.genero = genero;
+    }
+
+    public int getDuracionMinutos() {
+        return duracionMinutos;
+    }
+
+    public void setDuracionMinutos(int duracionMinutos) {
+        this.duracionMinutos = duracionMinutos;
+    }
 }
